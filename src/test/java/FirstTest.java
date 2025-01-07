f0a0441dd61d7d8bbb369c9b52c0c91c747ded7b
wkdan <jmy8876@gmail.com>
2025-01-07 17:29:50 +0900

    refactor: test blue

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -13,7 +13,7 @@ public class FirstTest {
     @DisplayName("앱 시작 시 '--- 수강신청 추천 시스템 --- 출력")
     void t1() {
 
-        String out = TestBot.run("종료");
+        String out = TestBot.run("");
         assertThat(out)
                 .containsSubsequence("--- 수강신청 추천 시스템 ---", "프로그램을 종료합니다.");
 
@@ -22,7 +22,14 @@ public class FirstTest {
     @Test
     @DisplayName("create - DB에 과목 정보 1개 입력")
     void t2() {
-        String out = TestBot.run("등록\n객체지향프로그래밍\nT043585\n3\n2\n없음\n수 1 2 금 3\n종료");
+
+        String out = TestBot.run("""
+                등록
+                객체지향프로그래밍
+                T043385
+                없음
+                수 1 2 금 3
+                """);
         assertThat(out)
                 .containsSubsequence("과목 명: ", "과목 코드: ","학점: ","학년: ","선수 과목: ", "수업 교시: ");
     }
