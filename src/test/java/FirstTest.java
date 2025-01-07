c6c7dc5507ebcb5ed443e1e62b8562640a2a89b1
wkdan <jmy8876@gmail.com>
2025-01-07 17:21:23 +0900

    refactor: t1 blue

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -10,17 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class FirstTest {
 
     @Test
-    @DisplayName("종료 테스트")
+    @DisplayName("앱 시작 시 '--- 수강신청 추천 시스템 --- 출력")
     void t1() {
-        Scanner sc = new Scanner("종료\n");
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-
-        App app = new App();
-        app.run();
-
-        assertThat(out.toString())
+        String out = TestBot.run("종료");
+        assertThat(out)
                 .containsSubsequence("--- 수강신청 추천 시스템 ---", "프로그램을 종료합니다.");
 
     }
