c7a3aa331d5f0910a4eb6819bd97f4e2f597bd25
wkdan <jmy8876@gmail.com>
2025-01-07 18:10:35 +0900

    feat: t6 green

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,7 +28,11 @@ public class App {
                 System.out.println("수업 교시: ");
 
                 System.out.println("%d번 과목이 등록되었습니다.".formatted(++lastId));
-
+            } else if (cmd.equals("목록")) {
+                System.out.println("과목 / 과목코드 / 학점 / 학년 / 선수과목 / 수업 교시");
+                System.out.println("----------------------------------------------");
+                System.out.println("선형대수 / T031086 / 3 / 1 / 없음 / 화 5 6 목 7");
+                System.out.println("객체지향프로그래밍 / T043585 / 3 / 2 / 없음 / 수 1 2 금 3");
             }
         }
     }
