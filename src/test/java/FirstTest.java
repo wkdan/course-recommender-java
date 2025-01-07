edd0b85a95ad44b910687ad70e2d93b56f048b29
wkdan <jmy8876@gmail.com>
2025-01-07 17:27:35 +0900

    test: t2 red

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -18,4 +18,12 @@ public class FirstTest {
                 .containsSubsequence("--- 수강신청 추천 시스템 ---", "프로그램을 종료합니다.");
 
     }
+
+    @Test
+    @DisplayName("create - DB에 과목 정보 1개 입력")
+    void t2() {
+        String out = TestBot.run("등록\n객체지향프로그래밍\nT043585\n3\n2\n없음\n수 1 2 금 3\n종료");
+        assertThat(out)
+                .containsSubsequence("과목 명: ", "과목 코드: ","학점: ","학년: ","선수 과목: ", "수업 교시: ");
+    }
 }
