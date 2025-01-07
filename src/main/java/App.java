7b27b4be2c27b407e84323d30a0bfd4f9d868b97
wkdan <jmy8876@gmail.com>
2025-01-07 17:33:04 +0900

    feat: t3 green

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,18 @@
 public class App {
     public void run() {
         System.out.println("--- 수강신청 추천 시스템 ---");
+        System.out.println("명령 ) ");
+        System.out.println("명령 ) ");
+        System.out.println("명령 ) ");
+        System.out.println("명령 ) ");
+
         System.out.println("과목 명: ");
         System.out.println("과목 코드: ");
         System.out.println("학점: ");
         System.out.println("학년: ");
         System.out.println("선수 과목: ");
         System.out.println("수업 교시: ");
+
         System.out.println("프로그램을 종료합니다.");
     }
 }
