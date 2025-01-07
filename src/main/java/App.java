59dfb98f97f887f5dd6e7643486c11089b421ed3
wkdan <jmy8876@gmail.com>
2025-01-07 17:27:50 +0900

    feat: t2 blue

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,12 @@
 public class App {
     public void run() {
         System.out.println("--- 수강신청 추천 시스템 ---");
+        System.out.println("과목 명: ");
+        System.out.println("과목 코드: ");
+        System.out.println("학점: ");
+        System.out.println("학년: ");
+        System.out.println("선수 과목: ");
+        System.out.println("수업 교시: ");
         System.out.println("프로그램을 종료합니다.");
     }
 }
