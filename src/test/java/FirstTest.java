8d066ce745688ef942d325bc5724685f98c97b2d
wkdan <jmy8876@gmail.com>
2025-01-07 17:17:13 +0900

    test: t1 red

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FirstTest {
+
+    @Test
+    @DisplayName("종료 테스트")
+    void t1() {
+        Scanner sc = new Scanner("종료\n");
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        App app = new App();
+        app.run();
+
+        assertThat(out.toString()).contains("--- 수강신청 추천 시스템 ---");
+        assertThat(out.toString()).contains("프로그램을 종료합니다.");
+
+    }
+}
